73f2984bff7722190d9cc2540ae90a86b0442715
BokaMokoena <102230320+BokaMokoena@users.noreply.github.com>
2024-05-20 13:03:58 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/service/ReviewService.java
@@ -0,0 +1,42 @@
+package za.ac.cput.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Review;
+import za.ac.cput.repository.ReviewRepository;
+
+import java.util.List;
+@Service
+public class ReviewService implements IReviewService{
+    private ReviewRepository repository;
+
+    @Autowired
+    ReviewService(ReviewRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Review save(Review review) {
+        return repository.save(review);
+    }
+
+    @Override
+    public Review read(String reviewId) {
+        return repository.findByReviewId(reviewId);
+    }
+
+    @Override
+    public boolean delete(String reviewId) {
+        Review review = repository.findByReviewId(reviewId);
+        if (review != null) {
+            repository.delete(review);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<Review> getAll() {
+        return repository.findAll();
+    }
+}
