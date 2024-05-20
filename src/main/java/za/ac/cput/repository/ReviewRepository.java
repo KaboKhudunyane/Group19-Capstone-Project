15bc980ad32f54bc0ad578ee7a98c6444f4bc9af
BokaMokoena <102230320+BokaMokoena@users.noreply.github.com>
2024-05-20 13:03:08 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package za.ac.cput.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.domain.Review;
+
+public interface ReviewRepository extends JpaRepository<Review, String> {
+    Review findByReviewId(String reviewId);
+}
+
