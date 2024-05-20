f3bc88758078260dda9f53c16c5bc1ac3de9ac03
Thenjiwembi <127989689+Thenjiwembi@users.noreply.github.com>
2024-05-20 16:26:13 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/service/CarInsuranceService.java
@@ -0,0 +1,44 @@
+package za.ac.cput.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.CarInsurance;
+import za.ac.cput.repository.CarInsuranceRepository;
+
+
+import java.util.List;
+
+  @Service
+public class CarInsuranceService implements IService<CarInsurance, String>{
+
+    private final CarInsuranceRepository repository;
+
+    @Autowired
+          public CarInsuranceService(CarInsuranceRepository repository) {
+              this.repository = repository;
+          }
+          @Override
+          public CarInsurance save(CarInsurance carInsurance) {
+              return repository.save(carInsurance);
+          }
+
+          @Override
+          public CarInsurance read(String carInsuranceId) {
+              return repository.findCarInsuranceByCarInsuranceId(carInsuranceId);
+          }
+
+          @Override
+          public boolean delete(String carInsuranceId) {
+              CarInsurance carInsurance = repository.findCarInsuranceByCarInsuranceId(carInsuranceId);
+              if (carInsurance != null) {
+                  repository.delete(carInsurance);
+                  return true;
+              }
+              return false;
+          }
+          @Override
+          public List<CarInsurance> getAll () {
+              return repository.findAll();
+          }
+
+}
