4ac0621b4602409471bfed50e9b7962a4a330a86
FARREL JORIS MBAZABOUA IZANGO <81972630+Jor65897@users.noreply.github.com>
2024-05-21 17:59:49 +0200

    Factory
    Factory classes for Address and Contacts added

---
+++ src/main/java/za/ac/cput/factory/AddressFactory.java
@@ -0,0 +1,22 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Address;
+import za.ac.cput.util.Helper;
+
+public class AddressFactory {
+
+    public static Address createAddress(String addressId, String userId, String streetName, String suburb, int postalCode) {
+        if (!Helper.isNullOrEmpty(addressId)||!Helper.isNullOrEmpty(userId)||!Helper.isNullOrEmpty(streetName)||!Helper.isNullOrEmpty(suburb)||!Helper.isNullOrEmpty(postalCode)) {
+           // throw new IllegalArgumentException("Invalid address ID");
+            return null;
+        }
+        return new Address.Builder()
+                .setAddressId(addressId)
+                .setUserId(userId)
+                .setStreetName(streetName)
+                .setSuburb(suburb)
+                .setPostalCode(postalCode)
+                .build();
+    }
+}
+
