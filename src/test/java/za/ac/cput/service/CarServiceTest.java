a29e75b34e7287fc17b586b062b0119a4099bda2
Kabo Khudunyane <88645384+KaboKhudunyane@users.noreply.github.com>
2024-05-26 18:04:37 +0200

    Add files via upload

--- src/test/java/za/ac/cput/service/CarServiceTest.java
+++ src/test/java/za/ac/cput/service/CarServiceTest.java
@@ -1,24 +1,41 @@
 package za.ac.cput.service;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import za.ac.cput.domain.Address;
-import za.ac.cput.domain.Contact;
-import za.ac.cput.domain.Name;
 import za.ac.cput.domain.Car;
+import za.ac.cput.domain.CarInformation;
 import za.ac.cput.factory.CarFactory;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 public class CarServiceTest {
 
     @Autowired
     private CarService carService;
-    private static
+
+    private static CarInformation carInformation = new CarInformation.Builder()
+            .setMake("Toyota")
+            .setModel("Corolla")
+            .setYear("2020")
+            .setLicensePlate("ABC123")
+            .setDescription("A reliable sedan")
+            .setFeatures("Air conditioning, Power windows")
+            .setCarId("123456")
+            .build();
+
+    private static Car car = new Car.Builder()
+            .setCarID("123")
+            .setUserID("456")
+            .setCarInformation(carInformation)
+            .setRate("100")
+            .setAvailability("Available")
+            .setStatus("Active")
+            .buildCar();
+
     @Test
     void testCreate() {
         Car createdCar = carService.create(car);
@@ -36,6 +53,7 @@ public class CarServiceTest {
         Car newCar = new Car.Builder()
                 .copyCar(car)
                 .setCarID("789")
+                .setRate("150")
                 .buildCar();
         Car updatedCar = carService.update(newCar);
         assertNotNull(updatedCar);
--- src/test/java/za/ac/cput/service/UserServiceTest.java
+++ src/test/java/za/ac/cput/service/UserServiceTest.java
@@ -1,24 +1,26 @@
 package za.ac.cput.service;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import za.ac.cput.domain.Address;
 import za.ac.cput.domain.Contact;
 import za.ac.cput.domain.Name;
 import za.ac.cput.domain.User;
+import za.ac.cput.factory.AddressFactory;
+import za.ac.cput.factory.ContactFactory;
+import za.ac.cput.factory.NameFactory;
 import za.ac.cput.factory.UserFactory;
 import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class UserServiceTest {
+
     @Autowired
     private UserService userService;
 
-    private static Name name;
-    private static Contact contact;
-    private static Address address;
+    private static Name name = NameFactory.createName("Kabo", "Kb", "Khudunyane");
+    private static Contact contact = ContactFactory.createContact("123" ,"216273293@mycput.ac.za",01234);
+    private static Address address = AddressFactory.createAddress("1", "216273293", "123 Street", "City", 12345);
 
     private static User user = UserFactory.createUser("216273293",
             name,
@@ -28,16 +30,22 @@ class UserServiceTest {
             "Admin",
             "Kabo.jpeg",
             true);
+
+    // Test case for creating a user
     @Test
     void create(){
         User createUser = userService.create(user);
+        assertNotNull(createUser);
+        System.out.println("Created User: " + createUser);
     }
+    // Test case for reading a user
     @Test
     void read(){
-        String readUser = userService.read(user.getUserID());
+        User readUser = userService.read(user.getUserID());
         assertNotNull(readUser);
-        System.out.println(readUser);
+        System.out.println("Read User: " + readUser);
     }
+    // Test case for updating a user
     @Test
     void update(){
         User newUser = new User.Builder()
@@ -46,10 +54,14 @@ class UserServiceTest {
                 .buildUser();
         User updatedUser = userService.update(newUser);
         assertNotNull(updatedUser);
-        System.out.println(updatedUser);
+        System.out.println("Updated User: " + updatedUser);
     }
+    // Test case for deleting a user
     @Test
     void delete(){
-        userService.delete();
+        userService.delete(user.getUserID());
+        User deletedUser = userService.read(user.getUserID());
+        assertNull(deletedUser);
+        System.out.println("User deleted successfully.");
     }
 }
