f1158d040ed1b9f7697e5df49fdfaf1f781fb477
FARREL JORIS MBAZABOUA IZANGO <81972630+Jor65897@users.noreply.github.com>
2024-05-26 22:56:43 +0200

    Tests for AddressControler and ContactController

---
+++ src/test/java/za/ac/cput/controller/AddressControllerTest.java
@@ -0,0 +1,28 @@
+package za.ac.cput.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressControllerTest {
+
+    @Test
+    void getAllAddresses() {
+    }
+
+    @Test
+    void getAddressById() {
+    }
+
+    @Test
+    void createAddress() {
+    }
+
+    @Test
+    void updateAddress() {
+    }
+
+    @Test
+    void deleteAddress() {
+    }
+}
