e46e5987cc47939c9be5f6540c5ddd8f012efa26
Kabo Khudunyane <88645384+KaboKhudunyane@users.noreply.github.com>
2024-05-26 18:09:50 +0200

    Add files via upload

--- src/main/java/za/ac/cput/service/IService.java
+++ src/main/java/za/ac/cput/service/IService.java
@@ -1,7 +1,10 @@
 package za.ac.cput.service;
-public interface IService <T, ID>{
 
-    T  save(T t);
-    ID read (ID id);
-
+public interface IService<T, ID> {
+    // Method to create a new entity
+    T create(T t);
+    // Method to read an entity by its ID
+    ID read(ID id);
+    // Method to save an existing entity
+    T update(T t);
 }
