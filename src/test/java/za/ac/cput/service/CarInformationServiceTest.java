7db57d02a5cd19d0de022c7e4b399c135f734562
Boka Mokoena <bokamokoena2@gmail.com>
2024-06-20 09:40:12 +0200

    Update CarInformationServiceTest.java

--- src/test/java/za/ac/cput/service/CarInformationServiceTest.java
+++ src/test/java/za/ac/cput/service/CarInformationServiceTest.java
@@ -3,7 +3,9 @@ package za.ac.cput.service;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import za.ac.cput.domain.Car;
 import za.ac.cput.domain.CarInformation;
+import za.ac.cput.domain.Review;
 import za.ac.cput.factory.CarInformationFactory;
 
 import java.util.List;
@@ -12,39 +14,43 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class CarInformationServiceTest {
+public class CarInformationServiceTest {
 
     @Autowired
     private CarInformationService carInformationService;
 
-    private static CarInformation carInformation1;
-    private static CarInformation carInformation2;
+    private static Car carId = new Car.Builder()
+            .setCarID("009")
+            .setRate("10 out 10")
+            .setAvailability("True")
+            .setStatus("available")
+            .buildCar();
+    private static CarInformation carInformation1  = CarInformationFactory.buildCarInformation("011", "BMW", "M4",
+            "2017", "CA123-143", "M performance", "Twin turbo", carId);
 
-    @BeforeEach
-    void setUp() {
-        carInformation1 = CarInformationFactory.buildCarInformation("011", "BMW", "M4",
-                "2017", "CA123-143", "M performance", "Twin turbo", "CAR01");
-        assertNotNull(carInformation1);
-        carInformation2 = CarInformationFactory.buildCarInformation("012", "Toyota", "Corolla",
-                "2012", "CA143-764", "Standard", "Single turbo", "CAR02");
-        assertNotNull(carInformation2);
-    }
 
     @Test
-    @Order(1)
-    void save() {
-        CarInformation created1 = carInformationService.save(carInformation1);
-        assertNotNull(created1);
-        System.out.println("Saved CarInformation 1: " + created1);
-        CarInformation created2 = carInformationService.save(carInformation2);
-        assertNotNull(created2);
-        System.out.println("Saved CarInformation 2: " + created2);
+    void create() {
+        CarInformation createdCarInformation = carInformationService.create(carInformation1);
+        assertNotNull(createdCarInformation);
+        System.out.println("Created Car: " + createdCarInformation);
     }
-
+    @Test
+    @Order(2)
+    void Update() {
+        CarInformation newCarInformation = new CarInformation.Builder()
+                .copyCarInformation(carInformation1)
+                .setCarInformationId("22")
+                .setMake("VW")
+                .buildCarInformation();
+        CarInformation updatedCarInformation = carInformationService.update(newCarInformation);
+        assertNotNull(updatedCarInformation);
+        System.out.println("Updated Car: " + updatedCarInformation);
+    }
     @Test
     @Order(2)
     void read() {
-        CarInformation read = carInformationService.read(carInformation2.getCarInformationId());
+        CarInformation read = carInformationService.read(carInformation1.getCarInformationId());
         assertNotNull(read);
         System.out.println("Read CarInformation: " + read);
     }
@@ -52,19 +58,16 @@ class CarInformationServiceTest {
     @Test
     @Order(3)
     void delete() {
-        boolean deleted = carInformationService.delete(carInformation1.getCarInformationId());
-        assertTrue(deleted);
-        System.out.println("Deleted CarInformation 1");
-    }
-
+            carInformationService.delete(carInformation1.getCarInformationId());
+            System.out.println("Car deleted successfully");
+        }
     @Test
     @Order(4)
-    void getAll() {
-        List<CarInformation> allCarInformation = carInformationService.getAll();
+    void getAllCarInformation() {
+        List<CarInformation> allCarInformation = carInformationService.getAllCarInformation();
         assertNotNull(allCarInformation);
         assertTrue(allCarInformation.size() > 0);
         System.out.println("All CarInformation: " + allCarInformation);
     }
-
 }
 
