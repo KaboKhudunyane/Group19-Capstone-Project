9ff4a693337781fd912e29cfd3c70acb6aff83f1
FARREL JORIS MBAZABOUA IZANGO <81972630+Jor65897@users.noreply.github.com>
2024-07-16 11:19:10 +0200

    Update BookingFactory.java

--- src/main/java/za/ac/cput/factory/BookingFactory.java
+++ src/main/java/za/ac/cput/factory/BookingFactory.java
@@ -1,16 +1,17 @@
 package za.ac.cput.factory;
 
 import za.ac.cput.domain.Booking;
+import za.ac.cput.domain.Car;
 import za.ac.cput.domain.CarInformation;
 import za.ac.cput.util.Helper;
 
 public class BookingFactory {
-    public static Booking buildBooking(String bookingId, String startDate, String returnDate, String pickUpLocation, String dropOffLocation, CarInformation carInformation, String status, double totalPrice){
-        if(Helper.isNullOrEmpty(bookingId) || Helper.isNullOrEmpty(startDate) || Helper.isNullOrEmpty(returnDate) || Helper.isNullOrEmpty(status) || carInformation == null
+    public static Booking buildBooking(String bookingId, Car car, String startDate, String returnDate, String pickUpLocation, String dropOffLocation, String status, double totalPrice){
+        if(Helper.isNullOrEmpty(bookingId) || Helper.isNullOrEmpty(startDate) || Helper.isNullOrEmpty(returnDate) || Helper.isNullOrEmpty(status) || car == null
                 || Helper.isNullOrEmpty(pickUpLocation) || Helper.isNullOrEmpty(dropOffLocation)|| totalPrice < 0 )
             return null;
 
-        return new Booking.Builder().setBookingId(bookingId).setStartDate(startDate).setReturnDate(returnDate).setPickUpLocation(pickUpLocation).setDropOffLocation(dropOffLocation).setCarInformation(carInformation)
+        return new Booking.Builder().setBookingId(bookingId).setCar(car).setStartDate(startDate).setReturnDate(returnDate).setPickUpLocation(pickUpLocation).setDropOffLocation(dropOffLocation)
                 .setStatus(status).setTotalPrice(totalPrice)
                 .build();
 
