36da4e52dce2c2410ea28e47ea332f6bbadb1c0c
FARREL JORIS MBAZABOUA IZANGO <81972630+Jor65897@users.noreply.github.com>
2024-07-16 11:28:25 +0200

    Update BookingFactoryTest.java

--- src/test/java/za/ac/cput/factory/BookingFactoryTest.java
+++ src/test/java/za/ac/cput/factory/BookingFactoryTest.java
@@ -2,16 +2,20 @@ package za.ac.cput.factory;
 
 import org.junit.jupiter.api.Test;
 import za.ac.cput.domain.Booking;
+import za.ac.cput.domain.Car;
 import za.ac.cput.domain.CarInformation;
+import za.ac.cput.domain.CarInsurance;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class BookingFactoryTest {
-    CarInformation carInformation = CarInformationFactory.buildCarInformation("011", "BMW", "M4", "2017", "CA 123-456",
-            "It is an M-performance", "800hps");
+    CarInformation carInformation = CarInformationFactory.buildCarInformation("11", "BMW", "M4",
+            "2018", "CA123-456", "M performance");
+    CarInsurance carInsurance = CarInsuranceFactory.buildCarInsurance("Mv332", "D55", "Outsurance", "Pmv6588");
+    Car car = CarFactory.buildCar(carInformation, carInsurance, "150", "Available", "New".getBytes());
 
-    Booking booking = BookingFactory.buildBooking("b101", "15-June-2024","20-June-2024","10 Hanover street, Cape Town, 8001",
-            "10 Hanover street, Cape Town, 8001",carInformation,"Blue BMW M4(Manual)", 24000);
+    Booking booking = BookingFactory.buildBooking("b101", car,"20-June-2024","10 Hanover street, Cape Town, 8001",
+            "10 Hanover street, Cape Town, 8001","Blue BMW M4(Manual)", "2400",2500.0);
 
     @Test
     void testBuildbooking(){
