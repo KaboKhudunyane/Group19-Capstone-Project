06a17e93cf190a1f8d61e4ba4ff04a969a093530
FARREL JORIS MBAZABOUA IZANGO <81972630+Jor65897@users.noreply.github.com>
2024-08-20 19:06:00 +0200

    Update AdminFactory.java
    + password parameter..

--- src/main/java/za/ac/cput/factory/AdminFactory.java
+++ src/main/java/za/ac/cput/factory/AdminFactory.java
@@ -6,17 +6,19 @@ import za.ac.cput.domain.Name;
 import za.ac.cput.util.Helper;
 
 public class AdminFactory {
-    public static Admin bluidAdmin(Name name, Contact contact){
+    public static Admin bluidAdmin(Name name, Contact contact,String password){
         if(Helper.isNullOrEmpty(name.getFirstName())||
                 Helper.isNullOrEmpty(name.getLastName())
                 ||Helper.isNullOrEmpty(name.getMiddleName())
                 ||Helper.isNullOrEmpty(contact.getEmail())
-                ||Helper.isNullOrEmpty(contact.getMobileNumber())){
+                ||Helper.isNullOrEmpty(contact.getMobileNumber())
+                ||Helper.isNullOrEmpty(password)){
             return null;
         }
         return new Admin.Builder()
                 .setName(name)
                 .setContact(contact)
+                .setPassword(password)
                 .buildAdmin();
     }
 }
