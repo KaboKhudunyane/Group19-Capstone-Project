19d863c9407d32c8ea895b06199f23a01e405034
FARREL JORIS MBAZABOUA IZANGO <81972630+Jor65897@users.noreply.github.com>
2024-08-20 19:09:42 +0200

    Update AdminFactoryTest.java
    -unused import  +password parameter

--- src/test/java/za/ac/cput/factory/AdminFactoryTest.java
+++ src/test/java/za/ac/cput/factory/AdminFactoryTest.java
@@ -1,9 +1,7 @@
 package za.ac.cput.factory;
 
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestTemplate;
-import org.springframework.boot.test.context.SpringBootTest;
 import za.ac.cput.domain.Admin;
 import za.ac.cput.domain.Contact;
 import za.ac.cput.domain.Name;
@@ -11,15 +9,12 @@ import za.ac.cput.domain.Name;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AdminFactoryTest {
-        Name name = new Name.Builder().setFirstName("Joden").setMiddleName("Nathe").setLastName("Garfield").buildName();
-        Contact contact = new Contact.Builder().setEmail("jJoden@example.com").setMobileNumber("564467").buildContact();
+    Name name = new Name.Builder().setFirstName("Joden").setMiddleName("Nathe").setLastName("Garfield").buildName();
+    Contact contact = new Contact.Builder().setEmail("jJoden@example.com").setMobileNumber("564467").buildContact();
 
     @Test
     void buildAdmin() {
-        Admin a = AdminFactory.bluidAdmin(name,contact) ;
+        Admin a = AdminFactory.bluidAdmin(name,contact,"J1234565") ;
         assertNotNull(a);
         System.out.println(a.toString());
     }
-
-
-}
