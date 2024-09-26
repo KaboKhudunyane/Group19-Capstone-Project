1ac69c5be70ecabe8a9cc31506ff59c87e9ebe77
FARREL JORIS MBAZABOUA IZANGO <81972630+Jor65897@users.noreply.github.com>
2024-09-27 00:24:58 +0200

    Update CarInformationController.java

--- src/main/java/za/ac/cput/controller/CarInformationController.java
+++ src/main/java/za/ac/cput/controller/CarInformationController.java
@@ -3,9 +3,7 @@ package za.ac.cput.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import za.ac.cput.domain.CarInformation;
-import za.ac.cput.service.BookingService;
 import za.ac.cput.service.CarInformationService;
-import za.ac.cput.service.CarInsuranceService;
 
 import java.util.List;
 
@@ -15,19 +13,11 @@ import java.util.List;
 @CrossOrigin
 public class CarInformationController {
 
-    private CarInsuranceService carInsuranceService;
-    private CarInformationService carInformationService;
-
     @Autowired
-    public CarInformationController(CarInsuranceService carInsuranceService, CarInformationService carInformationService) {
-        this.carInformationService = carInformationService;
-        this.carInsuranceService = carInsuranceService;
-
-    }
+    private CarInformationService carInformationService;
 
     @PostMapping("/create")
     public CarInformation create(@RequestBody CarInformation carInformation){
-        carInsuranceService.create(carInformation.getCarInsurance());
         return carInformationService.create(carInformation);
     }
 
@@ -36,6 +26,10 @@ public class CarInformationController {
         return carInformationService.getAll();
     }
 
+    @GetMapping("/count")
+    public long getCarCount() {
+        return carInformationService.countCars();
+    }
 }
 
 
