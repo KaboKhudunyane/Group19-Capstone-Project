8b8095acbfdc2f92a056e907d225ec03ab710838
FARREL JORIS MBAZABOUA IZANGO <81972630+Jor65897@users.noreply.github.com>
2024-09-27 00:19:15 +0200

    Update CarInformationRepository.java

--- src/main/java/za/ac/cput/repository/CarInformationRepository.java
+++ src/main/java/za/ac/cput/repository/CarInformationRepository.java
@@ -1,10 +1,12 @@
 package za.ac.cput.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.CarInformation;
 
 @Repository
 public interface CarInformationRepository extends JpaRepository<CarInformation, Long> {
 
-
+    @Query("SELECT COUNT(c) FROM CarInformation c")
+    long countCars();
 }
