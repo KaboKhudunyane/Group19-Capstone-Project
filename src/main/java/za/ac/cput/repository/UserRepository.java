65b38aa9ea49c225e4e9a14be850ac65a572bec9
Kabo Khudunyane <khudunyane64@gmail.com>
2024-10-17 15:55:56 +0200

    Update UserRepository.java

--- src/main/java/za/ac/cput/repository/UserRepository.java
+++ src/main/java/za/ac/cput/repository/UserRepository.java
@@ -1,21 +1,14 @@
 package za.ac.cput.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.User;
-
 import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByAccountUsername(String username);
-
-    Optional<User> findById(Long userID);  // Changed to Optional for null safety
-
-    void deleteById(Long userID);  // Updated method to delete by Id
-
+    Optional<User> findByUsername(String username);
+    Optional<User> findById(Long userID);
+    void deleteById(Long userID);
     @Query("SELECT COUNT(u) FROM User u")
     long countUser();
 }
