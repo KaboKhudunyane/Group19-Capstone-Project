86e592cc8e41113fc22f7a65ed38221f4332cb8c
Kabo Khudunyane <khudunyane64@gmail.com>
2024-10-17 15:58:20 +0200

    Update UserServiceTest.java

--- src/test/java/za/ac/cput/service/UserServiceTest.java
+++ src/test/java/za/ac/cput/service/UserServiceTest.java
@@ -1,5 +1,4 @@
 package za.ac.cput.service;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +16,13 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class UserServiceTest {
     @Autowired
     private UserService userService;
-
-    // Image paths
-    private static final String LICENSE_PICTURE_PATH = "C:\\Users\\Kabo Khudunyane\\Pictures\\IMG1.PNG";
-    private static final String ID_PICTURE_PATH = "C:\\Users\\Kabo Khudunyane\\Pictures\\IMG2.PNG";
-
+    private Name name;
+    private Contact contact;
+    private Address address;
     private byte[] compressImage(String filePath) {
         try {
             BufferedImage image = ImageIO.read(new File(filePath));
@@ -39,54 +35,40 @@ class UserServiceTest {
             return null;
         }
     }
-
+    private static final String LICENSE_PICTURE_PATH = "C:\\Users\\Kabo Khudunyane\\Pictures\\IMG1.PNG";
+    private static final String ID_PICTURE_PATH = "C:\\Users\\Kabo Khudunyane\\Pictures\\IMG2.PNG";
     private byte[] licensePicture;
     private byte[] idPicture;
-    private Account account;
-    private Name name;
-    private Contact contact;
-    private Address address;
     private User user;
-
     @BeforeEach
     void setUp() {
-        licensePicture = compressImage(LICENSE_PICTURE_PATH);
-        idPicture = compressImage(ID_PICTURE_PATH);
-
-        account = new Account.Builder().setUsername("Username").setPassword("password").buildAccount();
         name = NameFactory.createName("Thato", "Emeka", "Nwamadi");
         contact = ContactFactory.createContact("295732963@mycput.ac.za", "0654545212");
         address = AddressFactory.createAddress("89 St Marks", "District 10", "Cape Town", "Western Cape", "8000");
-
-        // Create user with role
-        user = UserFactory.createUser(account, name, contact, address, licensePicture, idPicture, User.Role.USER); // Set the role here
+        licensePicture = compressImage(LICENSE_PICTURE_PATH);
+        idPicture = compressImage(ID_PICTURE_PATH);
+        user = UserFactory.createUser(User.Role.USER,"Username", "password", name, contact, address, licensePicture, idPicture);
     }
-
     @Test
     void create() {
         User createdUser = userService.create(user);
         assertNotNull(createdUser);
         System.out.println("Created User: " + createdUser);
     }
-
     @Test
     void read() {
         User readUser = userService.read(user.getUserID());
         assertNotNull(readUser);
         System.out.println("Read User: " + readUser);
     }
-
     @Test
     void update() {
-        // Set a new license picture (or modify other details as needed)
-        byte[] newLicensePicture = compressImage(LICENSE_PICTURE_PATH); // Update the image path as needed
-
+        byte[] newLicensePicture = compressImage(LICENSE_PICTURE_PATH);
         User newUser = new User.Builder().copyUser(user).setLicense(newLicensePicture).buildUser();
         User updatedUser = userService.update(newUser);
         assertNotNull(updatedUser);
         System.out.println("Updated User: " + updatedUser);
     }
-
     @Test
     void delete() {
         userService.delete(user.getUserID());
@@ -94,7 +76,6 @@ class UserServiceTest {
         assertNull(deletedUser);
         System.out.println("User deleted successfully.");
     }
-
     @Test
     void testCount() {
         System.out.println("Number of Users: " + userService.countUser());
